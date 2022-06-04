3558651865bb52fdc081a83681e8d8ce2825b608
ben <ben.transinh@gmail.com>
2022-06-04 21:33:42 +0700

    lensson one

---
+++ 1.js
@@ -0,0 +1,23 @@
+// console.log('Tara')
+// // check loi in ra man
+// var bien1 = 10
+// console.log(bien1)
+
+// var x = 1 , y = 2 , z = "Hello";
+// var a = "Obama"
+// console.log(z + y + a)
+
+// function test1() {
+//     console.log(x + y + a)
+// }
+
+
+// test1()
+var diem = 5;
+if(diem <= 4) {
+    console.log('Duoi trung binh')
+} else {
+    console.log(' Tren trung binh')
+        
+    }
+
